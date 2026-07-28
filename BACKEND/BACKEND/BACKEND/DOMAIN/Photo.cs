638d74be8bc8457381faf86e5d609cc5b135f89f
Paul-Cezar Mardar <paulmardar@gmail.com>
2026-07-29 00:31:36 +0300

    domain addition

--- BACKEND/BACKEND/BACKEND/DOMAIN/Photo.cs
+++ BACKEND/BACKEND/BACKEND/DOMAIN/Photo.cs
@@ -7,10 +7,10 @@ namespace BACKEND.DOMAIN
     public class Photo : IEntity
     {
         public int Id { get; set; }
-
         public string Url { get; set; } = string.Empty;
         public (int length, int width) Dimensions { get; set; } = (0, 0);
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public List<string> Tags{ get; set; } = new List<string>();
 
     }
 }
