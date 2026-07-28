532118c0d8e696d9d860982dc83638910944bcf2
Paul-Cezar Mardar <paulmardar@gmail.com>
2026-07-29 00:57:50 +0300

    added repo interface

---
+++ BACKEND/BACKEND/BACKEND/REPOSITORY/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using BACKEND.DOMAIN;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BACKEND.REPOSITORY
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        T? GetById(int id);
+        IEnumerable<T> GetAll();
+        T Add(T entity);
+        bool Update(T entity);
+        bool Delete(int id);
+    }
+}
