7840ddf37875e447e5505faf0d7e30e26a297ad9
Paul-Cezar Mardar <paulmardar@gmail.com>
2026-07-29 22:43:44 +0300

    add in file repository

---
+++ BACKEND/BACKEND/BACKEND/REPOSITORY/InFileRepository.cs
@@ -0,0 +1,36 @@
+﻿using BACKEND.DOMAIN.Objects;
+using BACKEND.REPOSITORY;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BACKEND.REPOSITORY
+{
+    public class InFileRepository<T> : IRepository<T> where T : IEntity
+    {
+        public T Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
